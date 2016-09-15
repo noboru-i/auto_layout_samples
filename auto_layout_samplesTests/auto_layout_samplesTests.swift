1afd6e6212613b8cf5487988e898b553ddde0043
noboru-i <ishikura.noboru@gmail.com>
2016-09-15 22:38:05 +0900

    first commit

---
+++ auto_layout_samplesTests/auto_layout_samplesTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_samplesTests.swift
+//  auto_layout_samplesTests
+//
+//  Created by 石倉 昇 on 2016/09/15.
+//  Copyright © 2016年 noboru. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_samples
+
+class auto_layout_samplesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
