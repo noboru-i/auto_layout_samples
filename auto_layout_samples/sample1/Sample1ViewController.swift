e57d64c573c22e41fb1cd18c3d956bbd7f5ed532
noboru-i <ishikura.noboru@gmail.com>
2016-09-16 12:07:28 +0900

    sample implementation

--- auto_layout_samples/sample1/Sample1ViewController.swift
+++ auto_layout_samples/sample1/Sample1ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Sample1ViewController.swift
 //  auto_layout_samples
 //
 //  Created by 石倉 昇 on 2016/09/15.
@@ -8,19 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Sample1ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func moveToSample1(_ sender: AnyObject) {
+    
+    override func loadView() {
+        if let view = UINib(nibName: "Sample1ViewController", bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView {
+            self.view = view
+        }
     }
 }
-
